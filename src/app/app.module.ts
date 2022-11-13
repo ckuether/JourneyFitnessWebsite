d1b8a11779625f2bd442aa529da4294be3d96ef1
Corey Kuether <coreykuether1@gmail.com>
2022-11-13 09:14:45 -0700

    Adding Terms & Conditions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     UserManagementComponent,
-    PrivacyPolicyComponent
+    PrivacyPolicyComponent,
+    TermsConditionsComponent
   ],
   imports: [
     BrowserModule,
